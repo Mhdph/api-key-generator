d535c678a2f544261f7279546bd0579b17bbf78e
Mahdi <86263611+Mhdph@users.noreply.github.com>
2023-03-22 22:51:46 +0330

    create navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import {getServerSession} from 'next-auth';
+import Link from 'next/link';
+import {ThemeToggle} from './ThemeToggle';
+import {buttonVariants} from './ui/Button';
+import SignInButton from './ui/SignInButton';
+import SignOutButton from './ui/SignOutButton';
+
+const Navbar = async () => {
+  const session = await getServerSession();
+
+  return (
+    <div className='fixed top-0 left-0 right-0 z-50 flex h-20 items-center justify-between border-b border-slate-300 bg-white/75 shadow-sm backdrop-blur-sm dark:border-slate-700 dark:bg-slate-900/75'>
+      <div className='container mx-auto flex w-full max-w-7xl items-center justify-between'>
+        <Link href='/' className={buttonVariants({variant: 'link'})}>
+          Text Similarity v1.0
+        </Link>
+
+        <div className='md:hidden'>
+          <ThemeToggle />
+        </div>
+
+        <div className='hidden gap-4 md:flex'>
+          <ThemeToggle />
+          <Link href='/documentation' className={buttonVariants({variant: 'ghost'})}>
+            Documentation
+          </Link>
+          {session ? (
+            <>
+              <Link className={buttonVariants({variant: 'ghost'})} href='/dashboard'>
+                Dashboard
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <SignInButton />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
